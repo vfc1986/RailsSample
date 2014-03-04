2e2d24e67a9eec9ee8d0dade5d9bc58261951bf2
Fabrizio Valerio Covone <fabrizio.valerio.covone@gmail.com>
2014-03-04 16:33:08 +0000

    chapter 5 finished - before exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+   match '/signup', to: 'users#new', via: 'get'
+  
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
