777eefdb21e4e1e95350a9827fc82e65eb14c079
Fabrizio Valerio Covone <fabrizio.valerio.covone@gmail.com>
2014-03-10 15:53:26 +0000

    Chapter 8 done until 8.1.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin',  to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
